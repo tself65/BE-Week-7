5fa4cad891eb48529582f894babd7dcf01adbecc
tself65 <123394663+tself65@users.noreply.github.com>
2023-06-29 23:30:17 -0600

    first commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,30 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import projects.exception.DbException;
+
+public class DbConnection {
+	private static final String HOST = "localhost";
+	private static final String SCHEMA = "projects";
+	private static final String USER = "projects";
+	private static final String PASSWORD = "projects";
+	private static final int PORT = 3306;
+
+	public static Connection getConnection() {
+		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", 
+				HOST, PORT, SCHEMA, USER, PASSWORD);
+		
+		System.out.println("Connecting with url=" + uri); 
+		
+		try {
+			Connection conn = DriverManager.getConnection(uri);
+			System.out.println("Successfully obtained connection!"); 
+			return conn; 
+		} catch (SQLException e) {
+			System.out.println("Cannot obtain connection to " + uri);
+		  throw new DbException(e); 
+		} 
+	}
+}
