75a9e3a0dc60861b7853119b6c2d1dac14f2923f
tself65 <123394663+tself65@users.noreply.github.com>
2023-08-26 17:40:51 -0600

    Adding week 11 methods for Update and Delete

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -2,6 +2,8 @@ package projects.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import projects.exception.DbException;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
@@ -16,13 +18,31 @@ public class ProjectService {
 	}
 
 	public List<Project> fetchAllProjects() {
-		
-		return projectDao.fetchAllProjects();
+		// @formatter:off
+		return projectDao.fetchAllProjects()
+			.stream() 
+			.sorted((r1, r2) -> r1.getProjectId() - r2.getProjectId())
+			.collect(Collectors.toList());
+		// @formatter:on
 	}
 
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(
 				() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist.")); 
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		} 
+		
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+		
 	}
 
 }
